09387d80689020f3622cfb160c6533a349ddbd81
cecialvarez20 <cecilia.alvarez98@gmail.com>
2023-09-03 00:12:30 -0300

    Botón sin la alerta del div

--- eventos.js
+++ eventos.js
@@ -7,3 +7,7 @@
 document.querySelector('div').addEventListener("click", () => {
     alert ("Hola! Soy el div");
 });
+
+document.querySelector('button').addEventListener("click",  (e) => {
+    e.stopPropagation();
+});
